e9cc14d8202b28fb23ab4d1359f745d839ece866
Peter <petemit>
2017-01-21 20:35:37 -0600

    Got backend working.

--- app/src/main/java/com/mindbuilders/cognitivemoodlog/PositiveThoughtAddFragment.java
+++ app/src/main/java/com/mindbuilders/cognitivemoodlog/PositiveThoughtAddFragment.java
@@ -10,6 +10,7 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.SeekBar;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -27,16 +28,25 @@ public class PositiveThoughtAddFragment extends Fragment {
     ViewGroup posThoughtAdderList;
     CogMoodLogDatabaseHelper dbHelper;
     private List<CognitiveDistortionobj> cogobjs;
-    PositiveThoughtAddListener mPosThoughtAddListener;
+   // ThoughtAddFragment.ThoughtAddFragmentListener thoughtAddListener;
+    utilities util;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        util=new utilities(this.getContext());
         rootView = (ViewGroup) inflater.inflate(
                 R.layout.fragment_positive_thought_add, container, false);
         dbHelper=new CogMoodLogDatabaseHelper(this.getContext());
         thought_cognitivedistortionList=new ArrayList<thought_cognitivedistortionobj>();
         cogobjs=dbHelper.getCognitiveDistortionNameList();
+
+//        try {
+//            thoughtAddListener = (ThoughtAddFragment.ThoughtAddFragmentListener) this.getContext();
+//
+//        } catch (ClassCastException e) {
+//            throw new ClassCastException(this.getContext().toString());
+//        }
 
         posThoughtAdderList=(ViewGroup)rootView.findViewById(R.id.posThoughtAdderList);
         return rootView;
@@ -46,6 +56,7 @@ public class PositiveThoughtAddFragment extends Fragment {
 
     @Override
     public void setUserVisibleHint(boolean isVisibleToUser) {
+
         super.setUserVisibleHint(isVisibleToUser);
 
         if (isVisibleToUser && ((CreateNewLogEntry)getActivity()).getThoughtobjList()!=null &&((CreateNewLogEntry)getActivity()).getThought_cognitivedistortionobjs()!=null) {
@@ -55,12 +66,54 @@ public class PositiveThoughtAddFragment extends Fragment {
 
                     LayoutInflater inflater = LayoutInflater.from(rootView.getContext());
                     View tv = inflater.inflate(R.layout.positive_thought_adder_listitem, rootView, false);
-
+                    SeekBar seekbar = (SeekBar) tv.findViewById(R.id.posthought_sb);
+                    seekbar.setTag(tob.getId());
                     TextView thought = (TextView) tv.findViewById(R.id.posthoughtadder_negthought_tv);
                     thought.setText(tob.getNegativethought());
                     EditText posthought_et=(EditText)tv.findViewById(R.id.posthought_et);
+                    posthought_et.setTag(tob.getId());
                     thought_cognitivedistortionList=((CreateNewLogEntry)this.getActivity()).getThought_cognitivedistortionobjs();
                     //todo probably be better to convert these to hashtables or something...
+
+                    posthought_et.setOnFocusChangeListener(new View.OnFocusChangeListener(){
+                        @Override
+                        public void onFocusChange(View v, boolean hasFocus) {
+                            if (!hasFocus){
+                                util.hideKeyboard(v);
+                                for (thoughtobj tob: thoughtobjList) {
+                                    if (v.getTag()!=null&&tob.getId()==(int)v.getTag()){
+                                        tob.setPositivethought(((EditText)v).getText().toString());
+                                        Log.e("test","test2");//     thoughtAddListener.updateThoughts(thoughtobjList);
+                                    }
+                                }//endfor
+                            }
+
+                        }
+                    });
+
+                    seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+                        @Override
+                        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+                        }
+
+                        @Override
+                        public void onStartTrackingTouch(SeekBar seekBar) {
+
+                        }
+
+                        @Override
+                        public void onStopTrackingTouch(SeekBar seekBar) {
+                            rootView.hasFocus();
+                            for (thoughtobj tob: thoughtobjList) {
+                                if (seekBar.getTag()!=null&&tob.getId()==(int)seekBar.getTag()){
+                                    tob.setPositivebeliefbefore(seekBar.getProgress());
+                                }
+
+                            }
+                        }
+                    });
+
 
                     for (thought_cognitivedistortionobj tcdo :thought_cognitivedistortionList) {
 
@@ -71,7 +124,6 @@ public class PositiveThoughtAddFragment extends Fragment {
                                 if (cog.getId() == cid && cog.getId()>0) {
                                     //This appends the cognitive distortion to the Negative Thought Text
                                     thought.append("   (" + cog.getName() + ")");
-                                    Log.e("PosThoughtAddFragment",cog.getName());
                                 }
                             }
                         }
@@ -86,8 +138,10 @@ public class PositiveThoughtAddFragment extends Fragment {
 
     }
 
-    public interface PositiveThoughtAddListener {
-    }
+//    public interface PositiveThoughtAddListener {
+//        public void updatePositiveThoughtAddListList(List<thoughtobj> toblist);
+//
+//    }
 }
 
 
